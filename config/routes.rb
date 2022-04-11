cd74d91abd629f6e50536d397e1bac0ee87566ea
Luke Walsh <lhw2096@yahoo.com>
2022-04-10 23:07:47 -0500

    Create empty site controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'site/encode'
+  get 'site/decode'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
